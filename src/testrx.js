3f17640bc275df698775f23498a0f5200386ddc8
ganesh <ganeshdalei1992@gmail.com>
2020-06-25 01:24:42 +0530

    addmapfunction

--- src/testrx.js
+++ src/testrx.js
@@ -1,18 +1,16 @@
 import { Observable} from "rxjs";
 import { map, reduce, filter } from 'rxjs/operators';
 let stop = document.getElementById("stop");
+let add = document.getElementById("add");
+let Input = document.getElementById("text-input");
 var obser = Observable.create((observer) => {
-  setInterval(function () {
     observer.next("Hello");
-  }, 2000);
 });
 
 var observable = obser.subscribe(addList);
-
 stop.addEventListener("click", function () {
   observable.unsubscribe();
 });
-
 function addList(value) {
   var node = document.createElement("LI");
   var textnode = document.createTextNode(value);
@@ -20,3 +18,10 @@ function addList(value) {
   var ele = document.getElementById("render");
   ele.appendChild(node);
 }
+add.addEventListener("click", function(){
+ obser.pipe(
+    map(data =>data+ Input.value)
+  ).subscribe(addList)
+  
+  
+})
